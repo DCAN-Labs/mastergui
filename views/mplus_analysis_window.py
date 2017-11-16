12ecfc7420e93e437884647cb2527e2a3d8ee9e2
David Ball <davidrball@gmail.com>
2017-11-15 17:06:12 -0800

    multiple refactorings and ui additions to mplus analysis gui

--- views/mplus_analysis_window.py
+++ views/mplus_analysis_window.py
@@ -29,7 +29,7 @@ class MplusAnalysisWindow(AnalysisWindow):
             model.appendRow(item)
 
     def addInputColumnNamesToListViews(self):
-        cols = self.input.columnnames() + ["i", "q", "s", "r"]
+        cols = ["VOXEL","i", "q", "s", "r"] + self.input.columnnames()
         self.addColumnNamesToListView(self.columnSelectA, cols)
         self.addColumnNamesToListView(self.columnSelectB, cols)
 
@@ -79,6 +79,9 @@ class MplusAnalysisWindow(AnalysisWindow):
         self.model.add_rule(listA, operator, listB)
 
         self.ruleDisplay.setText(self.model.rules_to_s())
+
+        self.updateGeneratedMPlusInputFile()
+
 
     def createRuleOperatorWidget(self):
         group = QButtonGroup()
@@ -131,58 +134,76 @@ class MplusAnalysisWindow(AnalysisWindow):
 
         self.modelBuilderLayout.addWidget(self.ruleDisplay)
 
-    def initUISpecific(self):
+    def initModelBuilder(self):
+        self.modelBuilder = QWidget()
+        self.modelBuilderLayout = QVBoxLayout()
+        self.modelBuilderLayout.addWidget(QLabel("Select Covariates"))
+        self.modelBuilder.setLayout(self.modelBuilderLayout)
+        self.initModelBuilderPanel()
+
+    def addTopLevelOptionWidgets(self):
         self.analysisOptionsLayout.addWidget(QLabel("Missing Data Tokens:"))
         self.missingDataTokens = QLineEdit()
         self.missingDataTokens.setText(",".join(self.default_missing_tokens_list))
         self.analysisOptionsLayout.addWidget(self.missingDataTokens)
 
+        self.analysisOptionsLayout.addWidget(QLabel("Title:"))
+        self.titleEdit = QLineEdit()
+        self.titleEdit.setText("DefaultTitle")
+        self.analysisOptionsLayout.addWidget(self.titleEdit)
+
+    def initUISpecific(self):
+
+        self.addTopLevelOptionWidgets()
+
+        self.initModelBuilder()
+
         self.modelTemplateViewer = QTextEdit()
         self.generatedModelViewer = QTextEdit()
-        self.modelBuilder = QWidget()
-        self.modelBuilderLayout = QVBoxLayout()
-        self.modelBuilderLayout.addWidget(QLabel("Select Covariates"))
-        self.modelBuilder.setLayout(self.modelBuilderLayout)
+
         self.inputTable = QTableWidget()
         self.modelOutput = QTextEdit()
 
         self.tabs = QTabWidget()
 
-        x = QWidget()
+        newMplusModel = QWidget()
 
         self.modelDesignContainer = QVBoxLayout()
         self.modelDesignContainer.addWidget(self.generatedModelViewer)
-        x.setLayout(self.modelDesignContainer)
+        newMplusModel.setLayout(self.modelDesignContainer)
         self.tabs.resize(300, 200)
         self.tabs.addTab(self.inputTable, "Input Data Review")
         self.tabs.addTab(self.modelTemplateViewer, "Model Template")
         self.tabs.addTab(self.modelBuilder, "Model Builder")
-        self.tabs.addTab(x, "New Mplus Model")
+        self.tabs.addTab(newMplusModel, "New Mplus Model")
         self.tabs.addTab(self.modelOutput, "Raw MPlus Output")
         self.grid.addWidget(self.tabs)
 
-        self.initModelBuilderPanel()
+
 
     def updateUIAfterInput(self):
 
         self.addInputColumnNamesToListViews()
 
-    def Go(self):
-        # make data file with characters replaced
-        output_path = os.path.join(self.config._data.get("output_dir", ""), "GENERATEDmissing.csv")
-        self.input.save_cleaned_data(output_path, self.default_missing_tokens_list)
-        # make mplus analysi file
-        self.alert(output_path + " successfully saved.")
-
+    def updateGeneratedMPlusInputFile(self, save_to_path=""):
         columns = self.input.columnnames()
 
         self.model.set_column_names(columns)
 
         generated_mplus_model = self.model.to_string()
         self.generatedModelViewer.setText(generated_mplus_model)
-        # launch mplus
 
-        os.system('sox input.wav -b 24 output.aiff rate -v -L -b 90 48k')
+        if len(save_to_path)>0:
+            with open(save_to_path, "w") as f:
+                f.write(self.model.to_string())
+
+    def Go(self):
+        # make data file with characters replaced
+        output_path = os.path.join(self.config._data.get("output_dir", ""), "GENERATEDmissing.csv")
+        self.input.save_cleaned_data(output_path, self.default_missing_tokens_list)
+
+        #todo put in some MasterGui status/log box
+        # self.alert(output_path + " successfully saved.")
 
         # todo safety check this in case of rogue yml input!
 
@@ -190,9 +211,9 @@ class MplusAnalysisWindow(AnalysisWindow):
 
         model_output_file_path = model_input_file_path + ".out"
 
-        with open(model_input_file_path, "w") as f:
-            f.write(generated_mplus_model)
+        self.updateGeneratedMPlusInputFile(model_output_file_path)
 
+        # launch mplus
         cmd = self.config._data["MPlus_command"] + " " + model_input_file_path
 
         print("About to run command\n", cmd)
