063020b84afc12878e7db29b002997029dfdaa64
David Ball <davidrball@gmail.com>
2018-01-31 22:36:43 -0800

    new parsing techniques for output files

--- tests/test_mplus_output.py
+++ tests/test_mplus_output.py
@@ -15,5 +15,17 @@ class TestMplusModelOutput(unittest.TestCase):
 
         o.print_report()
 
+        print(o.warnings)
+
+    def test_dir_of_output(self):
+        p = "tests/"
+        output_set = MplusOutputSet(p)
+
+        stats = output_set.load_all()
+
+        print(stats)
+        print("why not preint stats")
+
+
 if __name__ == '__main__':
     unittest.main()
