5ab42665ff592e51bfc68f5d83e0cc0b19f1b2df
David Ball <davidrball@gmail.com>
2018-02-05 12:26:56 -0800

    utility to alter test data files, not part of app

---
+++ util/change_cifty_path.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+
+path = '../demo/roberttest1.csv'
+
+f = pd.read_csv(path)
+new_file_prefix = '/mnt/max/home/balld/testdata/'
+colindex = -1
+for row in range(f.shape[0]):
+    orig = f.iloc[row,colindex]
+    base = os.path.basename(orig)
+    new_path = os.path.join(new_file_prefix, base)
+    print(new_path)
+    f.iloc[row,colindex]=new_path
+
+f.to_csv(path + ".fixed", index=False)
+
+f.iloc[:5,:].to_csv(path+".5rows.fixed", index = False)
