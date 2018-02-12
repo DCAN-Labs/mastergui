b17164d4ff485bccd8a49e6e0eba82200b564cda
David Ball <davidrball@gmail.com>
2018-02-12 15:26:11 -0800

    restore global error handler for beta

--- views/mastergui_app.py
+++ views/mastergui_app.py
@@ -66,7 +66,7 @@ def excepthook(excType, excValue, tracebackobj):
     errorbox.exec_()
 
 # global exception handler, usually disabled during development but should be enabled in production
-#sys.excepthook = excepthook
+sys.excepthook = excepthook
 
 
 class MasterGuiApp(QMainWindow):
