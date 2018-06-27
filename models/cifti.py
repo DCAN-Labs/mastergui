f3a70644f3f2fba3fac8d2698144a549a836aaba
david_ball <balld@ohsu.edu>
2018-06-26 18:25:17 -0700

    fix requirement that the cifti source dir be writeable.  issue https://gitlab.com/Fair_lab/mastergui/issues/7

--- models/cifti.py
+++ models/cifti.py
@@ -3,15 +3,17 @@ import numpy as np
 import subprocess
 import time
 import pandas as pd
-
+import os
+import random
 
 class Cifti():
     def __init__(self, path, use_wbcommand=False):
-
+        """Loads a cifti using either nibabel or using workbench command to create a 
+        temporary file and read it in.  The workbench command method is much faster.
+        """
         # cift2.load returns a Cifti2Image
         # which inherits from a DataobjImage
         # http: // nipy.org / nibabel / reference / nibabel.cifti2.html  # nibabel.cifti2.cifti2.Cifti2Image
-
         if use_wbcommand:
             self.loadViaWbCommand(path)
         else:
@@ -22,7 +24,15 @@ class Cifti():
 
         # or wb_command -nifti-information -print-matrix base_output.dscalar.nii
         i = 0
-        output_path = path + ".csv"
+        #It needs to write a temporary csv file of just the numbers.
+        #the source dir of the ciftis is not necessarily writeable to the
+        #user running this so it writes that temporary csv to the users home directory
+        #and deletes it shortly later.  A random number is prepended to the filename
+        #to avoid name collisions
+        output_filename = str(random.randint(1,100000000)) + "." + os.path.basename(path)+".csv"
+
+        output_path = os.path.expanduser(os.path.join("~",output_filename))
+
         start_time = time.time()
 
         result = subprocess.run(["wb_command", "-cifti-convert", "-to-text", path, output_path])
--- models/cifti_matrix.py
+++ models/cifti_matrix.py
@@ -4,7 +4,7 @@ import subprocess
 import time
 import pandas as pd
 import os
-
+import random
 
 class CiftiMatrix():
     def __init__(self, path, wb_command_path_prefix):
@@ -14,22 +14,31 @@ class CiftiMatrix():
         # cift2.load returns a Cifti2Image
         # which inherits from a DataobjImage
         # http: // nipy.org / nibabel / reference / nibabel.cifti2.html  # nibabel.cifti2.cifti2.Cifti2Image
+       
+        #It needs to write a temporary csv file of just the numbers.
+        #the source dir of the ciftis is not necessarily writeable to the 
+        #user running this so it writes that temporary csv to the users home directory
+        #and deletes it shortly later.  A random number is prepended to the filename
+        #to avoid name collisions 
+        output_filename = str(random.randint(1,100000000)) + "." + os.path.basename(path)+".csv"
 
-        output_path = path + ".csv"
+        output_path = os.path.expanduser(os.path.join("~",output_filename))
         start_time = time.time()
 
         wb_command_text = os.path.join(self.wb_command_path_prefix, "wb_command")
-        print(wb_command_text)
-        result = subprocess.run([wb_command_text, "-cifti-convert", "-to-text", path, output_path])
+        try:
+            result = subprocess.run([wb_command_text, "-cifti-convert", "-to-text", path, output_path])
 
-        # VERY important to not let pandas infer a header row here,
-        # could cause on off-by-one error.
-        # there is no header row in the output of the cifti-convert command!
-        data_file = pd.read_csv(output_path, header=None)
+            # VERY important to not let pandas infer a header row here,
+            # could cause on off-by-one error.
+            # there is no header row in the output of the cifti-convert command!
+            data_file = pd.read_csv(output_path, header=None)
 
-        os.remove(output_path)
+            os.remove(output_path)
 
-        self.data = data_file[data_file.columns[0]]
+            self.data = data_file[data_file.columns[0]]
+        except:
+            raise ValueError("Error using workbench command to read cifti data at %s, perhaps you do not have write permissions in this location." % path)
 
     @property
     def matrix(self):
